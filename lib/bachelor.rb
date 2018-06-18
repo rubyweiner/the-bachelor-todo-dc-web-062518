f7bebbd3e92d483ba122d27ecebab658f37cf007
Ruby Weiner <rubyweiner796@me.com>
2018-06-18 22:15:10 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -10,7 +10,7 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  data[occupation].each do |contestant|
+  data[occupation].each do |contestant, |
     contestant.each do |k, v|
       if v == occupation 
         return contestant[:name]
