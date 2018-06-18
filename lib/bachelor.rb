6165e1a356b3257616726a84e8c76289080d8dba
Ruby Weiner <rubyweiner796@me.com>
2018-06-18 22:17:52 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -22,15 +22,15 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  person = []
+  contestants = []
   data.each do |hash_season, contestants_array|
     contestants_array.each do |contestant|
       if contestant["hometown"] == hometown
-        person << hash_season
+        contestants << hash_season
       end
     end
   end
-  person.length
+  contestants.length
 
 end
 
