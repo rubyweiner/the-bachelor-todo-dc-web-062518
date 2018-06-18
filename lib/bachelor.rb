39cfd3b3dc54868478906a50e32024e543d619bf
Ruby Weiner <rubyweiner796@me.com>
2018-06-18 22:10:21 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
-  data[season.to_sym].each do |into_array|
-   if into_array[:status] =="Winner"
-     return into_array[:name].split(" ")[0]
+  data[season.to_sym].each do |contestant|
+   if contestant[:status] =="Winner"
+     return contestant[:name].split(" ")[0]
    end
  end
 end
