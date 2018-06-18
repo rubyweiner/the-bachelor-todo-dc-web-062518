6f0b3dfb268cbb5b4a533663cabce140276c1d42
Ruby Weiner <rubyweiner796@me.com>
2018-06-18 22:22:12 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -35,15 +35,15 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  person = nil
-  data.each do |hash_season, contestants_array|
-    contestants_array.each do |contestant|
-      if contestant["hometown"] == hometown
-        person = contestant["occupation"]
-      end
-    end
-  end
-  person
+  data.each do |season, array|
++    array.each do |hash|
++      hash.each do |k,v|
++        if v == hometown
++          return hash["occupation"]
++        end
++      end
++    end
++  end
 end
 
 def get_average_age_for_season(data, season)
